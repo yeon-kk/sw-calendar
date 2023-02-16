df868dc30db7b1d1aa23887576957464df8b2727
yeonkyoung <keep.ykk@gmail.com>
2023-02-16 19:07:51 +0900

    Feat: 프리온보딩 유무 boolean 타입 추가

--- src/constant/academy.tsx
+++ src/constant/academy.tsx
@@ -2,6 +2,7 @@ interface academyType {
   title: string;
   end: string;
   url: string;
+  pre: boolean;
 }
 export const MONTH = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 export const ACADEMY_LIST: academyType[] = [
@@ -9,80 +10,96 @@ export const ACADEMY_LIST: academyType[] = [
     title: '네이버부스트캠프(AI Tech)',
     end: '1',
     url: 'https://boostcamp.connect.or.kr/',
+    pre: false,
   },
   {
     title: 'sw사관학교 정글',
     end: '1',
     url: 'https://swjungle.net/',
+    pre: true,
   },
   {
     title: '크래프톤 정글',
     end: '1',
     url: 'https://jungle.krafton.com/',
+    pre: true,
   },
   {
     title: 'SW마에스트로',
     end: '2',
     url: 'https://www.swmaestro.org/sw/main/main.do',
+    pre: false,
   },
   {
     title: 'SK쉴더스 루키즈',
     end: '2',
     url: 'https://sslc.kr/',
+    pre: false,
   },
   {
     title: '우아한테크캠프',
     end: '5',
     url: 'https://techblog.woowahan.com/8154/?utm_source=boottent&utm_medium=referral',
+    pre: false,
   },
   {
     title: '42서울',
     end: '6',
     url: 'https://42seoul.kr/seoul42/main/view',
+    pre: false,
   },
   {
     title: '삼성SDS',
     end: '6',
     url: '',
+    pre: false,
   },
   {
     title: '네이버부스트캠프',
     end: '6',
     url: 'https://boostcamp.connect.or.kr/',
+    pre: false,
   },
   {
     title: '우아한테크코스',
     end: '10',
     url: 'https://woowacourse.github.io/',
+    pre: true,
   },
   {
     title: '스마일게이트',
     end: '10',
     url: 'https://careers.smilegate.com/student/development-camp?utm_source=boottent&utm_medium=referral',
+    pre: false,
   },
   {
     title: '현대자동차그룹',
     end: '10',
     url: 'https://softeerbootcamp.com/',
+    pre: false,
   },
   {
     title: 'SK하이닉스',
     end: '10',
     url: 'https://sharing.skhynix.com/',
+    pre: false,
   },
   {
     title: 'SSAFY(싸피)',
     end: '11',
     url: 'https://www.ssafy.com/ksp/jsp/swp/swpMain.jsp',
+    pre: false,
   },
   {
     title: 'Apple',
     end: '11',
     url: 'https://developeracademy.postech.ac.kr/',
+    pre: false,
   },
   {
     title: 'KT 에이블스쿨',
     end: '12',
     url: 'https://aivle.kt.co.kr/home/main/indexMain',
+    pre: false,
   },
 ];
