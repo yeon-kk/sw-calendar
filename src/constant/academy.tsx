d6d08a82b31b2d0da3063fa3cf0709f1206e5631
yeonkyoung <keep.ykk@gmail.com>
2023-02-16 17:39:22 +0900

    Feat: 월, 교육 리스트 상수화

---
+++ src/constant/academy.tsx
@@ -0,0 +1,83 @@
+interface academyType {
+  title: string;
+  end: string;
+  url: string;
+}
+export const MONTH = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+export const ACADEMY_LIST: academyType[] = [
+  {
+    title: 'sw사관학교 정글',
+    end: '1',
+    url: 'https://swjungle.net/',
+  },
+  {
+    title: '크래프톤 정글',
+    end: '1',
+    url: 'https://jungle.krafton.com/',
+  },
+  {
+    title: 'SW마에스트로',
+    end: '2',
+    url: 'https://www.swmaestro.org/sw/main/main.do',
+  },
+  {
+    title: 'SK쉴더스 루키즈',
+    end: '2',
+    url: 'https://sslc.kr/',
+  },
+  {
+    title: '우아한테크캠프',
+    end: '5',
+    url: 'https://techblog.woowahan.com/8154/?utm_source=boottent&utm_medium=referral',
+  },
+  {
+    title: '42서울',
+    end: '6',
+    url: 'https://42seoul.kr/seoul42/main/view',
+  },
+  {
+    title: '삼성SDS',
+    end: '6',
+    url: '',
+  },
+  {
+    title: '네이버부스트캠프',
+    end: '6',
+    url: 'https://boostcamp.connect.or.kr/',
+  },
+  {
+    title: '우아한테크코스',
+    end: '10',
+    url: 'https://woowacourse.github.io/',
+  },
+  {
+    title: '스마일게이트',
+    end: '10',
+    url: 'https://careers.smilegate.com/student/development-camp?utm_source=boottent&utm_medium=referral',
+  },
+  {
+    title: '현대자동차그룹',
+    end: '10',
+    url: 'https://softeerbootcamp.com/',
+  },
+  {
+    title: 'SK하이닉스',
+    end: '10',
+    url: 'https://sharing.skhynix.com/',
+  },
+  {
+    title: 'SSAFY(싸피)',
+    end: '11',
+    url: 'https://www.ssafy.com/ksp/jsp/swp/swpMain.jsp',
+  },
+  {
+    title: 'Apple',
+    end: '11',
+    url: 'https://developeracademy.postech.ac.kr/',
+  },
+  {
+    title: 'KT 에이블스쿨',
+    end: '12',
+    url: 'https://aivle.kt.co.kr/home/main/indexMain',
+  },
+];
