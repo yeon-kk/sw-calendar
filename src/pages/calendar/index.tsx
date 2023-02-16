78ff7576ca0831c7a6827ba7eb8f92fc048376fc
yeonkyoung <keep.ykk@gmail.com>
2023-02-16 18:29:55 +0900

    Feat: year 추가

--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -1,9 +1,12 @@
 import { ACADEMY_LIST, MONTH } from '../../constant/academy';
 import styles from './Calendar.module.css';
 
+const YEAR = 2023;
+
 const Calendar = () => {
   return (
-    <article>
+    <article className={styles.calenderContainerLayout}>
+      <h2 className={styles.calenderYear}>{YEAR}년</h2>
       <div className={styles.calenderLayout}>
         {MONTH.map((value) => (
           <div className={styles.item}>
