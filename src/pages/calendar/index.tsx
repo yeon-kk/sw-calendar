4bce547cc1fc8a32434e0fe897c06080aa70a065
yeonkyoung <keep.ykk@gmail.com>
2023-02-17 16:37:41 +0900

    Feat: UX를 위한 UI 수정

--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -1,27 +1,73 @@
-import { ACADEMY_LIST, MONTH } from '../../constant/academy';
+import {
+  ACADEMY_LIST,
+  CREW_LIST,
+  MONTH,
+  MONTH_ACADEMY,
+  MONTH_CREW,
+  YEAR,
+  EXPLAIN,
+  CREW_LABEL,
+  ACADEMY_LABEL,
+  PRE_LABEL,
+  ALARM_LABEL,
+  YEAR_LABEL,
+  MONTH_LABEL,
+} from '../../constant/academy';
 import styles from './Calendar.module.css';
 
-const YEAR = 2023;
-const EXPLAIN = '대략적인 월단위 표시이기 때문에 꼭 확인하셔야 합니다.';
 const Calendar = () => {
   return (
     <article className={styles.calenderContainerLayout}>
-      <h2 className={styles.calenderYear}>{YEAR}년</h2>
-      <div className={styles.explain}>{EXPLAIN}</div>
+      <h2 className={styles.calenderYear}>
+        {YEAR}
+        {YEAR_LABEL}
+      </h2>
+      <div className={styles.explain}>
+        {EXPLAIN}
+        <div className={styles.indexContainer}>
+          <div className={`${styles.pre} ${styles.blue}`} />
+          <sub>{PRE_LABEL}</sub>
+          <div className={`${styles.pre} ${styles.yellow}`} />
+          <sub>{ALARM_LABEL}</sub>
+        </div>
+      </div>
       <div className={styles.calenderLayout}>
         {MONTH.map((value) => (
           <div className={styles.item}>
-            <h3 className={styles.month}>{value}월</h3>
-            {ACADEMY_LIST.map(
-              (academy) =>
-                parseInt(academy.end) === value && (
-                  <div className={styles.rowContainer}>
-                    <div
-                      className={`${styles.pre} ${academy.pre && styles.blue}`}
-                    />
-                    <a href={academy.url}>{academy.title}</a>
-                  </div>
-                )
+            <h3 className={styles.month}>
+              {value}
+              {MONTH_LABEL}
+            </h3>
+            {MONTH_ACADEMY[value - 1] && <span>{ACADEMY_LABEL}</span>}
+            {ACADEMY_LIST.map(({ title, end, url, pre, alarm }) =>
+              end.map(
+                (month) =>
+                  month === value && (
+                    <div className={styles.rowContainer}>
+                      <div className={`${styles.pre} ${pre && styles.blue}`} />
+                      <div
+                        className={`${styles.pre} ${alarm && styles.yellow}`}
+                      />
+                      <a href={url}>{title}</a>
+                    </div>
+                  )
+              )
+            )}
+            {MONTH_ACADEMY[value - 1] && <br />}
+            {MONTH_CREW[value - 1] && <span>{CREW_LABEL}</span>}
+            {CREW_LIST.map(({ title, end, url, pre, alarm }) =>
+              end.map(
+                (month) =>
+                  month === value && (
+                    <div className={styles.rowContainer}>
+                      <div className={`${styles.pre} ${pre && styles.blue}`} />
+                      <div
+                        className={`${styles.pre} ${alarm && styles.yellow}`}
+                      />
+                      <a href={url}>{title}</a>
+                    </div>
+                  )
+              )
             )}
           </div>
         ))}
