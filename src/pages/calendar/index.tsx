cbfaa054b70e90910d8e8576177646f1b19d81d6
yeonkyoung <keep.ykk@gmail.com>
2023-02-20 20:08:37 +0900

    Style: 월 표시 부분을 색으로 구분

--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -34,45 +34,53 @@ const Calendar = () => {
       <div className={styles.calenderLayout}>
         {MONTH.map((value) => (
           <div className={styles.item}>
-            <h3 className={styles.month}>
-              {value}
-              {MONTH_LABEL}
-            </h3>
-            {MONTH_ACADEMY[value - 1] && <span>{ACADEMY_LABEL}</span>}
-            {ACADEMY_LIST.map(({ title, end, url, pre, alarm }) =>
-              end.map(
-                (month) =>
-                  month === value && (
-                    <div className={styles.rowContainer}>
-                      <div className={`${styles.pre} ${pre && styles.blue}`} />
-                      <div
-                        className={`${styles.pre} ${alarm && styles.yellow}`}
-                      />
-                      <a href={url} target="_blank">
-                        {title}
-                      </a>
-                    </div>
-                  )
-              )
-            )}
-            {MONTH_ACADEMY[value - 1] && <br />}
-            {MONTH_CREW[value - 1] && <span>{CREW_LABEL}</span>}
-            {CREW_LIST.map(({ title, end, url, pre, alarm }) =>
-              end.map(
-                (month) =>
-                  month === value && (
-                    <div className={styles.rowContainer}>
-                      <div className={`${styles.pre} ${pre && styles.blue}`} />
-                      <div
-                        className={`${styles.pre} ${alarm && styles.yellow}`}
-                      />
-                      <a href={url} target="_blank">
-                        {title}
-                      </a>
-                    </div>
-                  )
-              )
-            )}
+            <div className={styles.monthLabel}>
+              <h3 className={styles.month}>
+                {value}
+                {MONTH_LABEL}
+              </h3>
+            </div>
+            <div className={styles.monthContents}>
+              {MONTH_ACADEMY[value - 1] && <span>{ACADEMY_LABEL}</span>}
+              {ACADEMY_LIST.map(({ title, end, url, pre, alarm }) =>
+                end.map(
+                  (month) =>
+                    month === value && (
+                      <div className={styles.rowContainer}>
+                        <div
+                          className={`${styles.pre} ${pre && styles.blue}`}
+                        />
+                        <div
+                          className={`${styles.pre} ${alarm && styles.yellow}`}
+                        />
+                        <a href={url} target="_blank">
+                          {title}
+                        </a>
+                      </div>
+                    )
+                )
+              )}
+              {MONTH_ACADEMY[value - 1] && <br />}
+              {MONTH_CREW[value - 1] && <span>{CREW_LABEL}</span>}
+              {CREW_LIST.map(({ title, end, url, pre, alarm }) =>
+                end.map(
+                  (month) =>
+                    month === value && (
+                      <div className={styles.rowContainer}>
+                        <div
+                          className={`${styles.pre} ${pre && styles.blue}`}
+                        />
+                        <div
+                          className={`${styles.pre} ${alarm && styles.yellow}`}
+                        />
+                        <a href={url} target="_blank">
+                          {title}
+                        </a>
+                      </div>
+                    )
+                )
+              )}
+            </div>
           </div>
         ))}
       </div>
